1c6699e03cbfd063ee85b0961552ad0d23131718
catally <catally@gmail.com>
2019-02-11 23:51:16 +0800

    Add init for P

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -50,6 +50,10 @@ FusionEKF::FusionEKF() {
               0, 1, 0, 0;
 
   ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1000, 0,
+             0, 0, 0, 1000;
 
   ekf_.Q_ = MatrixXd(4, 4);
   
